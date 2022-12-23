07ec11a4d98130997cf41c556248ad4732b132bd
prashant_handel <prashanthandel2501@gmail.com>
2022-12-23 21:42:30 +0530

    Initialize Project

---
+++ script.js
@@ -0,0 +1,188 @@
+let snakeBody = [{x: 4, y: 5}];
+let timer = true;
+
+let gameBoardEl = document.querySelector(".gameBoard");
+let lastPaintTime = 0;
+let speed = 5;
+let inputDirection = {x: 0, y: 1}
+let applePosition = {x: 5, y: 8}
+const scoreEl = document.getElementById("score");
+let score = 0;
+const gameoverImg = document.querySelector(".gameover");
+let isPaused = false;
+let playBtn = document.getElementById("play");
+let isGameOver =false;
+const foodMusic = new Audio('food.mp3');
+const gameOverMusic = new Audio('gameover.mp3');
+const moveMusic = new Audio('move.mp3');
+const music = new Audio('music.mp3');
+const musicOnBtn = document.getElementById("musicOnBtn");
+const musicOffBtn = document.getElementById("musicOffBtn");
+
+// Game Function
+function main(ctime){
+    if(!isPaused){
+    window.requestAnimationFrame(main);
+    if(((ctime - lastPaintTime)/1000) < 1/speed){
+    return;
+    }
+    lastPaintTime = ctime;
+    update();
+    }
+}
+
+// Calling for request animation frame for the first time
+window.requestAnimationFrame(main);
+
+// Music on function
+musicOnBtn.addEventListener("click", ()=>{
+    music.play();
+});
+
+// Music off function
+musicOffBtn.addEventListener("click", ()=>{
+    music.pause();
+});
+
+function drawSnake(){
+    snakeBody.forEach((element, index) =>{
+        let snakeElement = document.createElement("div");
+        snakeElement.style.gridColumnStart = element.x;
+        snakeElement.style.gridRowStart = element.y;
+        if(index == 0){
+            snakeElement.classList.add('head');
+        }
+        else{
+            snakeElement.classList.add('snake');
+        }
+        gameBoardEl.appendChild(snakeElement);
+    })
+}
+
+// Updating snake according to movement 
+function 
+update(){
+    undraw();
+    drawSnake();
+    appleShow();
+    eatApple();
+    snakeMove();
+    gameover();
+}
+
+// Moving snake in any direction
+function snakeMove(){
+    // moving whole body of the in a line
+    for(i = snakeBody.length - 2; i >= 0; i--){
+    snakeBody[i+1] = {...snakeBody[i]};
+}
+
+    snakeBody[0].x += inputDirection.x;
+    snakeBody[0].y += inputDirection.y;
+}
+
+function moveUp(){
+    inputDirection = {x: 0, y: -1}
+}
+function moveDown(){
+    inputDirection = {x: 0, y: 1}
+}
+function moveLeft(){
+    inputDirection = {x: -1, y: 0}
+}
+function moveRight(){
+    inputDirection = {x: 1, y: 0}
+}
+
+function control(e) {
+    if (timer) {
+      if (e.keyCode === 37 && inputDirection.x != 1) {
+        moveLeft();
+        moveMusic.play();
+    } else if (e.keyCode === 38 && inputDirection.y != 1) {
+        moveUp();
+        moveMusic.play();
+    } else if (e.keyCode === 39 && inputDirection.x != -1) {
+        moveRight();
+        moveMusic.play();
+    } else if (e.keyCode === 40 && inputDirection.y != -1) {
+        moveDown();
+        moveMusic.play();
+      }
+    }
+  }
+  window.addEventListener("keydown", control);
+
+// Clearing board every time
+function undraw(){
+    gameBoardEl.innerHTML = "";
+}
+
+// show  apple for the first time
+function appleShow(){
+    let apple = document.createElement('div');
+    apple.style.gridColumnStart = applePosition.x;
+    apple.style.gridRowStart = applePosition.y;
+    apple.classList.add('apple');
+    gameBoardEl.appendChild(apple);
+}
+
+// apple eating function and incresing score and adding elements to the snake
+function eatApple(){
+    if(applePosition.x == snakeBody[0].x && applePosition.y == snakeBody[0].y){
+        foodMusic.play();
+        // assigning new position to the apple
+        applePosition = {x: Math.ceil(Math.random() * 16), y: Math.ceil(Math.random() * 16)};
+
+        // add element to the snake
+        snakeBody.push(snakeBody.length);
+
+        // increasing speed
+        if(speed<= 10){
+        speed++;
+        }   
+        // update score
+        score++;
+        scoreEl.innerHTML = score;
+}
+}
+// for collision and gameover
+
+function gameover(){
+    for(i=1; i< snakeBody.length; i++){
+        if(snakeBody[0].x == snakeBody[i].x && snakeBody[0].y == snakeBody[i].y){
+            gameoverImg.style.display = 'flex';
+            inputDirection = {x: 0, y: 0}
+            isPaused = true;
+            gameOverMusic.play();
+            isGameOver = true;
+        }    
+    }
+    if(snakeBody[0].x <= 0 || snakeBody[0].x >= 17 || snakeBody[0].y <= 0 || snakeBody[0].y >= 17){
+        gameoverImg.style.display = 'flex';
+        inputDirection = {x: 0, y: 0}
+        isPaused = true;
+        gameOverMusic.play();
+        isGameOver = true;
+    }
+}
+
+// Add functionality to paly/ pause button
+playBtn.addEventListener("click",()=>{
+    if(isGameOver){
+        window.requestAnimationFrame(main);
+        location.reload(true);
+        inputDirection = {x: 0, y: 1}
+        applePosition = {x: 5, y: 8}
+        snakeBody = [{x: 4, y: 5}];
+    }
+    else if(!isPaused){
+        timer = true;
+        isPaused = true;
+    }
+    else if(isPaused){
+        timer = false;
+        isPaused = false;
+        window.requestAnimationFrame(main);
+    }
+})
